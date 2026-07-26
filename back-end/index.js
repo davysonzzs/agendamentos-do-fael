0f7b79997e4002d8d067d6007692e9f61838b67b
davyson <davyssonlevi2.0@gmail.com>
2026-07-25 21:30:23 -0300

    atualizando o cors

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {cors: {origin: "http://localhost:5173"}});
+const io = require('socket.io')(server, {cors: {origin: "https://agendamentos-do-fael.vercel.app/"}});
 io.on('connection', socket => {
     console.log("usuario " + socket.id + " conectado")
 
